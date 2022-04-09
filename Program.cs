63af5da0b7eacd56238fb13f2883ce01ef15de33
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-04-09 09:36:00 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+using System.Diagnostics;
+
+
+
+namespace modul7_1302204080
+{
+    public class program
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
